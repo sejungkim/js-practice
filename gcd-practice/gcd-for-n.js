941002816ca22c209ce78e8547166574405217ee
sejungkim <smilesejung@gmail.com>
2018-03-27 16:11:25 +0900

    Add folder for gcd practice files

---
+++ gcd-practice/gcd-for-n.js
@@ -0,0 +1,23 @@
+function gcd() {
+  var divisorArray = [],
+    minNum;
+
+  minNum = Math.min(...arguments);
+  for (var i = minNum; i > 0; i--) {
+    if (minNum % i === 0) {
+      divisorArray.push(i);
+    }
+  }
+  for (var i = 0; i < divisorArray.length; i++) {
+    for (var j = 0; j < arguments.length; j++) {
+      if (arguments[j] % divisorArray[i] !== 0) {
+        break;
+      }
+      if (j === (arguments.length - 1)) {
+        return divisorArray[i];
+      }
+    }
+  }
+}
+
+console.log(gcd(84, 120));
---
+++ gcd-practice/gcd-for-two.js
@@ -0,0 +1,36 @@
+function divisors(num) {
+  var result = [], divNum = num;
+  for (var i = 0; divNum > 0; i++) {
+    if (num % divNum === 0) {
+      result[i] = divNum;
+    }
+    else {
+      i--;
+    }
+    divNum--;
+  }
+  return result;
+}
+
+function gcd() {
+  var inputNumlen = arguments.length, divisorNum = {};
+  var divisorNumlen = [], short = 0, long = 1;
+  var greatest;
+  for (var i = 0; i < inputNumlen; i++) {
+    divisorNum[i] = divisors(arguments[i]);
+    divisorNumlen[i] = divisorNum[i].length;
+  }
+  if (divisorNumlen[0] > divisorNumlen[1]) {
+    short = 1;
+    long = 0;
+  }
+  for (var i = 0; i < divisorNumlen[short]; i++) {
+    if (divisorNum[long].includes(divisorNum[short][i])) {
+      greatest = divisorNum[short][i];
+      break;
+    }
+  }
+  console.log(greatest);
+}
+
+gcd(84, 120);
