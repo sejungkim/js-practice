55fe33378af0b5c491248a6872914944d64952ca
sejungkim <smilesejung@gmail.com>
2018-04-06 18:47:00 +0900

    Update trimmedArray function with arrow function

--- pipe-function/example2.js
+++ pipe-function/example2.js
@@ -1,11 +1,6 @@
 var s = 'todo$공부하기 , todo$알고리즘공부 , doing$스터디하기';
 
 const split = str => str.split(",");
-
-function trimmedArray(arr) {
-  return arr.map(function (val) {
-    return val.trim();
-  });
-}
+const trimmedArray = arr => arr.map(val => val.trim());
 
 console.log(trimmedArray(split(s)));
