63f5233b877ef265ed51418e2a3fb553f23e0fbe
sejungkim <smilesejung@gmail.com>
2018-04-06 18:40:16 +0900

    Make trimmedArray function

--- pipe-function/example2.js
+++ pipe-function/example2.js
@@ -2,4 +2,10 @@ var s = 'todo$공부하기 , todo$알고리즘공부 , doing$스터디하기';
 
 const split = str => str.split(",");
 
-console.log(split(s));
+function trimmedArray(arr) {
+  return arr.map(function (val) {
+    return val.trim();
+  });
+}
+
+console.log(trimmedArray(split(s)));
