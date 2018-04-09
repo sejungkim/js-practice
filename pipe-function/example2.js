fc087a384509aea2396c47c9ceef5aa0dde74ddc
sejungkim <smilesejung@gmail.com>
2018-04-09 18:56:08 +0900

    Update makeTodoObject function with arrow function

--- pipe-function/example2.js
+++ pipe-function/example2.js
@@ -3,13 +3,11 @@ const s = 'todo$공부하기 , todo$알고리즘공부 , doing$스터디하기';
 const split = str => str.split(",");
 const trimmedArray = arr => arr.map(val => val.trim());
 
-function makeTodoObject(arr) {
-  return arr.reduce((obj, target) => {
-    const keyAndVal = target.split('$');
-    obj[keyAndVal[0]] = obj[keyAndVal[0]] || [];
-    obj[keyAndVal[0]].push(keyAndVal[1]);
-    return obj;
-  }, {});
-}
+const makeTodoObject = arr => arr.reduce((obj, target) => {
+  const keyAndVal = target.split('$');
+  obj[keyAndVal[0]] = obj[keyAndVal[0]] || [];
+  obj[keyAndVal[0]].push(keyAndVal[1]);
+  return obj;
+}, {});
 
 console.log(makeTodoObject(trimmedArray(split(s))));
