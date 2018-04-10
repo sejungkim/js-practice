b18850e54893d684e94487af16f12f98a99ac108
sejungkim <smilesejung@gmail.com>
2018-04-10 12:12:32 +0900

    Update doubleNum and delayPrint with arrow function

--- pipe-function/example1.js
+++ pipe-function/example1.js
@@ -1,11 +1,5 @@
+const doubleNum = num => num * 2;
+const delayPrint = num => setTimeout(() => console.log(num), 1000);
 const pipe = (...funs) => value => funs.reduce((val, fun) => fun(val), value);
 
 pipe(parseInt, doubleNum, delayPrint)('2018');
-
-function doubleNum(num) {
-  return num * 2;
-}
-
-function delayPrint(num) {
-  setTimeout(() => console.log(num), 1000);
-}
